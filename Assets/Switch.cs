052b29ed7dab12d1c6f7300a3976710e14d904b5
daniel-lee-user <danielhlee02@gmail.com>
2020-01-04 19:55:06 -0800

    First Commit
    Neuron Models controllable with mouse and keyboard

---
+++ Assets/Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+
+    public static int select = 0;
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Q))
+        {
+            if(select == 1)
+            {
+                select--;
+            } else
+            {
+                select++;
+            }
+        }
+	}
+}
---
+++ Assets/rotateAndScale.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateAndScale : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+        Switch.select = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Switch.select == 0)
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                transform.localScale += new Vector3(0.001f, 0.001f, 0.001f);
+            }
+            else if (Input.GetKey(KeyCode.N))
+            {
+                transform.localScale -= new Vector3(0.001f, 0.001f, 0.001f);
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                //transform.Rotate(Vector3.up, -5 * Time.deltaTime);
+                transform.Rotate(100 * Time.deltaTime, 1 * Time.deltaTime, 1 * Time.deltaTime);
+
+            }
+            else if (Input.GetKey(KeyCode.K))
+            {
+                transform.Rotate(-100 * Time.deltaTime, 1 * Time.deltaTime, 1 * Time.deltaTime);
+                //transform.Rotate(Vector3.up, 5 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.Y))
+            {
+                //transform.Rotate(Vector3.up, -5 * Time.deltaTime);
+                transform.Rotate(1 * Time.deltaTime, 100 * Time.deltaTime, 1 * Time.deltaTime);
+
+            }
+            else if (Input.GetKey(KeyCode.U))
+            {
+                transform.Rotate(1 * Time.deltaTime, -100 * Time.deltaTime, 1 * Time.deltaTime);
+                //transform.Rotate(Vector3.up, 5 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.H))
+            {
+                //transform.Rotate(Vector3.up, -5 * Time.deltaTime);
+                transform.Rotate(1 * Time.deltaTime, 1 * Time.deltaTime, 100 * Time.deltaTime);
+
+            }
+            else if (Input.GetKey(KeyCode.J))
+            {
+                transform.Rotate(1 * Time.deltaTime, 1 * Time.deltaTime, -100 * Time.deltaTime);
+                //transform.Rotate(Vector3.up, 5 * Time.deltaTime);
+            }
+        }
+        Debug.Log(Switch.select + " og");
+    }
+}
